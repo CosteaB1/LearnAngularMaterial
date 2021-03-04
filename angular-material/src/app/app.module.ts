97b139550816446bbafe68a51ea3967af8697ccd
Constantin Bivol <constantin.bivol@amdaris.com>
2021-03-04 17:45:52 +0200

    adeed smth

--- angular-material/src/app/app.module.ts
+++ angular-material/src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
+import { MaterialModule } from './material/material.module';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { LayoutComponent } from './layout/layout.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
